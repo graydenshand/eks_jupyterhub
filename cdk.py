4f59b8714f4caff058a5e61e3a75a0d143d2c9a1
Grayden Shand <graydenshand@gmail.com>
2023-08-29 21:41:22 -0400

    Provide example of deploying a custom image

--- cdk.py
+++ cdk.py
@@ -1,43 +1,37 @@
+from typing import Self
+
 import aws_cdk as cdk
 import aws_cdk.aws_ec2 as ec2
+import aws_cdk.aws_ecr_assets as ecr_assets
 import aws_cdk.aws_eks as eks
 import aws_cdk.aws_iam as iam
 import yaml
-from constructs import Construct
-from typing import Self
 from aws_cdk.lambda_layer_kubectl import KubectlLayer
+from constructs import Construct
 
+# These constants are set for a transient "dev" deployment
 REMOVAL_POLICY = cdk.RemovalPolicy.DESTROY
 DELETION_PROTECTION = False
 
 
-class JupyterHubStack(cdk.Stack):
+class JupyterhubStack(cdk.Stack):
     def __init__(
         self, scope: Construct, id: str, vpc_id: str | None = None, masters_role_arn: str | None = None, **kwargs
     ) -> Self:
         super().__init__(scope, id, **kwargs)
 
-        if masters_role_arn is None:
-            masters_role = iam.Role(
-                self,
-                "MastersRole",
-                assumed_by=iam.AccountPrincipal(self.account),
-            )
-        else:
-            masters_role = iam.Role.from_role_arn(self, "MastersRole", masters_role_arn)
+        masters_role = iam.Role(
+            self,
+            "MastersRole",
+            assumed_by=iam.AccountPrincipal(self.account),
+        )
 
-        if vpc_id is None:
-            vpc = ec2.Vpc(self, "Vpc")
-        else:
-            vpc = ec2.Vpc.from_lookup(vpc_id=vpc_id)
-
-        # provisioning a cluster
+        # Provision a Kubernetes cluster
         cluster = eks.Cluster(
             self,
             "K8sCluster",
             version=eks.KubernetesVersion.V1_27,
             kubectl_layer=KubectlLayer(self, "kubectl-layer"),
-            vpc=vpc,
             masters_role=masters_role,
             output_masters_role_arn=True,
             output_cluster_name=True,
@@ -51,6 +45,14 @@ class JupyterHubStack(cdk.Stack):
                 eks.ClusterLoggingTypes.AUTHENTICATOR,
                 eks.ClusterLoggingTypes.CONTROLLER_MANAGER,
             ],
+        )
+
+        # Grant masters role necessary permissions
+        masters_role.add_to_policy(
+            iam.PolicyStatement(
+                actions=["eks:AccessKubernetesApi", "eks:Describe*", "eks:List*"],
+                resources=[cluster.cluster_arn],
+            )
         )
 
         # Add EBS CSI driver addon
@@ -82,30 +84,55 @@ class JupyterHubStack(cdk.Stack):
             cluster_name=cluster.cluster_name,
             service_account_role_arn=ebs_csi_addon_role.role_arn,
         )
+        ebs_csi_addon.apply_removal_policy(REMOVAL_POLICY)
 
-        # Grant masters role necessary permissions
-        masters_role.add_to_policy(
-            iam.PolicyStatement(
-                actions=["eks:AccessKubernetesApi", "eks:Describe*", "eks:List*"],
-                resources=[cluster.cluster_arn],
-            )
+        # Build and deploy custom docker image
+        image = ecr_assets.DockerImageAsset(
+            self,
+            "UserServerBaseImage",
+            directory=".",
+            platform=ecr_assets.Platform.LINUX_AMD64,
+        )
+        cdk.CfnOutput(
+            self,
+            "ImageUri",
+            value=image.image_uri,
+            description="URI of image deployed to ECR repository.",
         )
 
-        # Deploy Jupyterhub helm chart
+        # Parse config for Jupyterhub helm chart
         with open("config.yaml", "r") as f:
-            eks.HelmChart(
-                self,
-                "JupyterHubHelmChart",
-                cluster=cluster,
-                chart="jupyterhub",
-                repository="https://jupyterhub.github.io/helm-chart/",
-                namespace="jupyterhub",
-                create_namespace=True,
-                release="jupyterhub",
-                version="3.0.1",
-                wait=True,
-                values=yaml.load(f, Loader=yaml.Loader),
-            )
+            config = yaml.load(f, Loader=yaml.Loader)
+
+        # Add our custom image to the helm chart config
+        config["singleuser"]["image"] = {
+            "name": image.repository.repository_uri,
+            "tag": image.image_tag,
+        }
+
+        # Create a K8s secret to grant permissions to pull from private ECR registry
+        config["imagePullSecret"] = {
+            "create": True,
+            "registry": image.repository.repository_uri,
+            "username": "aws",
+            "email": "__token__",
+            "password": "aws ecr get-login-password --region us-east-1 | cut -d' ' -f6",
+        }
+
+        # Deploy Jupyterhub helm chart
+        eks.HelmChart(
+            self,
+            "JupyterHubHelmChart",
+            cluster=cluster,
+            chart="jupyterhub",
+            repository="https://jupyterhub.github.io/helm-chart/",
+            namespace="jupyterhub",
+            create_namespace=True,
+            release="jupyterhub",
+            version="3.0.2",
+            wait=True,
+            values=config,
+        )
 
         # Expose service endpoint as stack output
         jupyterhub_endpoint = cluster.get_service_load_balancer_address("proxy-public", namespace="jupyterhub")
@@ -120,7 +147,7 @@ class JupyterHubStack(cdk.Stack):
 if __name__ == "__main__":
     app = cdk.App()
 
-    JupyterHubStack(
+    JupyterhubStack(
         app,
         "EksJupyterhub",
         termination_protection=DELETION_PROTECTION,
