20401d0c0c75defce7dbcbc46ae0ab433f051b63
Grayden Shand <graydenshand@gmail.com>
2023-09-03 15:51:00 -0400

    Working EFS based deployment

--- cdk.py
+++ cdk.py
@@ -3,6 +3,7 @@ from typing import Self
 import aws_cdk as cdk
 import aws_cdk.aws_ec2 as ec2
 import aws_cdk.aws_ecr_assets as ecr_assets
+import aws_cdk.aws_efs as efs
 import aws_cdk.aws_eks as eks
 import aws_cdk.aws_iam as iam
 import yaml
@@ -12,6 +13,7 @@ from constructs import Construct
 # These constants are set for a transient "dev" deployment
 REMOVAL_POLICY = cdk.RemovalPolicy.DESTROY
 DELETION_PROTECTION = False
+AUTOMATIC_BACKUPS = False
 
 
 class JupyterhubStack(cdk.Stack):
@@ -45,6 +47,7 @@ class JupyterhubStack(cdk.Stack):
                 eks.ClusterLoggingTypes.AUTHENTICATOR,
                 eks.ClusterLoggingTypes.CONTROLLER_MANAGER,
             ],
+            service_ipv4_cidr="172.20.0.0/16",
         )
 
         # Grant masters role necessary permissions
@@ -55,36 +58,88 @@ class JupyterhubStack(cdk.Stack):
             )
         )
 
-        # Add EBS CSI driver addon
+        # Add EFS CSI driver addon
         oid_connect_issuer_id = cluster.open_id_connect_provider.open_id_connect_provider_issuer.replace("https://", "")
-        ebs_csi_addon_role_policy_condition = cdk.CfnJson(
+        efs_csi_addon_role_policy_condition = cdk.CfnJson(
             self,
-            "K8sEbsAddonPolicyCondition",
+            "K8sEfsAddonPolicyCondition",
             value={
                 f"{oid_connect_issuer_id}:aud": "sts.amazonaws.com",
-                f"{oid_connect_issuer_id}:sub": "system:serviceaccount:kube-system:ebs-csi-controller-sa",
+                f"{oid_connect_issuer_id}:sub": "system:serviceaccount:kube-system:efs-csi-*",
             },
         )
-        ebs_csi_addon_role = iam.Role(
+        efs_csi_addon_role = iam.Role(
             self,
-            "K8sEbsAddonRole",
+            "K8sEfsAddonRole",
             assumed_by=iam.FederatedPrincipal(
                 federated=cluster.open_id_connect_provider.open_id_connect_provider_arn,
-                conditions={"StringEquals": ebs_csi_addon_role_policy_condition},
+                conditions={"StringLike": efs_csi_addon_role_policy_condition},
                 assume_role_action="sts:AssumeRoleWithWebIdentity",
             ),
         )
-        ebs_csi_addon_role.add_managed_policy(
-            iam.ManagedPolicy.from_aws_managed_policy_name("service-role/AmazonEBSCSIDriverPolicy")
+        efs_csi_addon_role.add_managed_policy(
+            iam.ManagedPolicy.from_aws_managed_policy_name("service-role/AmazonEFSCSIDriverPolicy")
         )
-        ebs_csi_addon = eks.CfnAddon(
+        efs_csi_addon = eks.CfnAddon(
             self,
-            "K8sEbsCsiAddon",
-            addon_name="aws-ebs-csi-driver",
+            "K8sEfsCsiAddon",
+            addon_name="aws-efs-csi-driver",
             cluster_name=cluster.cluster_name,
-            service_account_role_arn=ebs_csi_addon_role.role_arn,
+            service_account_role_arn=efs_csi_addon_role.role_arn,
         )
-        ebs_csi_addon.apply_removal_policy(REMOVAL_POLICY)
+        efs_csi_addon.apply_removal_policy(REMOVAL_POLICY)
+
+        efs_security_group = ec2.SecurityGroup(
+            self,
+            "EfsSecurityGroup",
+            vpc=cluster.vpc,
+            allow_all_outbound=True,
+        )
+        efs_security_group.add_ingress_rule(
+            ec2.Peer.ipv4(cluster.vpc.vpc_cidr_block),
+            ec2.Port.tcp(2049),
+            description="Allow all inbound NFS traffic from VPC.",
+        )
+        efs_security_group.add_ingress_rule(
+            ec2.Peer.ipv4("172.20.0.0/16"),
+            ec2.Port.tcp(2049),
+            description="Allow all inbound NFS traffic from EKS cluster.",
+        )
+        file_system = efs.FileSystem(
+            self,
+            "FileSystem",
+            vpc=cluster.vpc,
+            lifecycle_policy=efs.LifecyclePolicy.AFTER_14_DAYS,
+            out_of_infrequent_access_policy=efs.OutOfInfrequentAccessPolicy.AFTER_1_ACCESS,
+            removal_policy=REMOVAL_POLICY,
+            security_group=efs_security_group,
+            enable_automatic_backups=AUTOMATIC_BACKUPS,
+        )
+
+        eks_namespace = cluster.add_manifest(
+            "EksNamespace",
+            {
+                "apiVersion": "v1",
+                "kind": "Namespace",
+                "metadata": {"name": "jupyterhub"},
+            },
+        )
+
+        efs_storage_class = cluster.add_manifest(
+            "EfsStorageClass",
+            {
+                "apiVersion": "storage.k8s.io/v1",
+                "kind": "StorageClass",
+                "metadata": {"name": "efs", "namespace": "jupyterhub"},
+                "provisioner": "efs.csi.aws.com",
+                "parameters": {
+                    "provisioningMode": "efs-ap",
+                    "fileSystemId": file_system.file_system_id,
+                    "directoryPerms": "700",
+                },
+            },
+        )
+        efs_storage_class.node.add_dependency(eks_namespace)
 
         # Build and deploy custom docker image
         image = ecr_assets.DockerImageAsset(
@@ -119,8 +174,8 @@ class JupyterhubStack(cdk.Stack):
             "password": "aws ecr get-login-password --region us-east-1 | cut -d' ' -f6",
         }
 
-        # Deploy Jupyterhub helm chart
-        eks.HelmChart(
+        # # Deploy Jupyterhub helm chart
+        jupyterhub = eks.HelmChart(
             self,
             "JupyterHubHelmChart",
             cluster=cluster,
@@ -129,10 +184,11 @@ class JupyterhubStack(cdk.Stack):
             namespace="jupyterhub",
             create_namespace=True,
             release="jupyterhub",
-            version="3.0.2",
+            version="3.0.3",
             wait=True,
             values=config,
         )
+        jupyterhub.node.add_dependency(efs_storage_class)
 
         # Expose service endpoint as stack output
         jupyterhub_endpoint = cluster.get_service_load_balancer_address("proxy-public", namespace="jupyterhub")
